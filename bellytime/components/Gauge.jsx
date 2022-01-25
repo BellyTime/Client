e9cf29043da35353c904e9b3b7396e6f8877edb9
eunsun <nammugmuhg@gmail.com>
2022-01-25 21:35:56 +0900

    [#26]feat:게이지 넣기

--- bellytime/components/Gauge.jsx
+++ bellytime/components/Gauge.jsx
@@ -1,22 +1,34 @@
-
-
+import { arc } from "d3-shape";
+import { scaleLinear } from "d3-scale";
 const Gauge = ({ value = 50, min = 0, max = 100, label, units }) => {
-    const arcGenerator = d3
-      .arc()
-      .innerRadius(25)
-      .outerRadius(40)
-      .startAngle(0)
-      .endAngle(5.5)
-      .padAngle(0)
-      .cornerRadius(20);
-    const arcPath = arcPathGenerator();
+  const backgroundArc = arc()
+    .innerRadius(0.65)
+    .outerRadius(1)
+    .startAngle(-Math.PI / 2)
+    .endAngle(Math.PI / 2)
+    .cornerRadius(1)();
+  const percentScale = scaleLinear().domain([min, max]).range([0, 1]);
+  const percent = percentScale(value);
+  const angleScale = scaleLinear()
+    .domain([0, 1])
+    .range([-Math.PI / 2, Math.PI / 2])
+    .clamp(true);
+  const angle = angleScale(percent);
+  const filledArc = arc()
+    .innerRadius(0.65)
+    .outerRadius(1)
+    .startAngle(-Math.PI / 2)
+    .endAngle(angle)
+    .cornerRadius(1)();
   return (
     <div>
       <svg
         width="9em"
         viewBox={[-1, -1, 2, 1].join(" ")}
         className="border-solid border-4 border-pink-600 hover:border-dotted"
-      ></svg>
+      >
+        <path d={backgroundArc} fill="#dbdbe7" />
+      </svg>
     </div>
   );
 };
