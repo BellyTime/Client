0588b36768dec9a4ad5340783b9cf35c44b49839
eunsun <nammugmuhg@gmail.com>
2022-02-03 06:13:45 +0900

    [#26] chore: 파일위치 조정

--- bellytime/state/atom.js
+++ bellytime/state/atom.js
@@ -3,13 +3,24 @@ export const modalState = atom({
   key: "modalState", // unique ID (with respect to other atoms/selectors)
   default: false, // default value (aka initial value)
 });
-
-export const modifyState = atom({
-  key: "modifyState", // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
-});
+//모달상태 : 닫힌상태, 열린상태, 삭제, 수정, 등록
 
 export const modalContentState = atom({
   key: "modalContentState", // unique ID (with respect to other atoms/selectors)
   default: "", // default value (aka initial value)
+});
+
+export const alertState = atom({
+  key: "alertModalState", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+
+export const setCoolTimeState = atom({
+  key: "setCoolTimeState", // unique ID (with respect to other atoms/selectors)
+  default: {
+    foodId: "",
+    foodName: "",
+    startDate: "",
+    duration: 0
+  }, // default value (aka initial value)
 });
