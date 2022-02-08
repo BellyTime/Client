1fd0347ef0445f718c0ac9fbd95c01ee69097c81
eunsun <nammugmuhg@gmail.com>
2022-02-08 20:44:19 +0900

    [#33]feat:예약관리 데이터 가져오기

---
+++ bellytime/components/reservation/ReservedShop.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+import { drawCanvas } from "../followingShop/drawCanvas";
+export const ReservedShop = ({ content }) => {
+  const { shopId, shopName, profileImg, reservedDate, dDay, personnel } =
+    content;
+  const canvasRef = useRef();
+  const imgRef = useRef();
+  useEffect(() => {
+    drawCanvas(100, 100, canvasRef, imgRef, profileImg);
+  }, []);
+  return (
+    <div>
+      <img ref={imgRef} />
+      <canvas ref={canvasRef} />
+      <p>{shopName}</p>
+    </div>
+  );
+};
