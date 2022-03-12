d2827e16fc1a6072dcd8e741d1ee7f4dc258815b
eunsun <nammugmuhg@gmail.com>
2022-03-13 03:58:12 +0900

    [#4]feat:쿠키있는지 유무

---
+++ bellytime/util/getCookie.js
@@ -0,0 +1,23 @@
+function getCookie2(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+
+export default function getCookie(cookie, name) {
+  const q = {};
+  cookie
+    ?.replace(/\s/g, "")
+    .split(";")
+    .map((i) => i.split("="))
+    .forEach(([key, value]) => {
+      q[key] = value;
+    });
+  return q[name] ?? null;
+}
