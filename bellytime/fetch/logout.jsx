4dbd265964c246953e739f9e8135cccec2d0a295
eunsun <nammugmuhg@gmail.com>
2022-03-14 04:32:05 +0900

    [#4]feat:로그아웃 서버요청

--- bellytime/fetch/logout.jsx
+++ bellytime/fetch/logout.jsx
@@ -4,7 +4,7 @@ import { axiosInstance } from "./instance";
 export const logout = async () => {
   const URL = `/logout`;
   try {
-    await axios.get(URL);
+    await axiosInstance.get(URL);
     // console.log(response.data.accessToken);
     // window.localStorage.setItem("accessToken", response.data.accessToken);
   } catch (e) {
