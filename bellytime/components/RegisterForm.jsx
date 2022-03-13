6892a80c89cdf17dca0b9af64d18857d773378fa
eunsun <nammugmuhg@gmail.com>
2022-03-14 07:04:23 +0900

    [#4]feat:로그인 완료

--- bellytime/components/RegisterForm.jsx
+++ bellytime/components/RegisterForm.jsx
@@ -1,92 +1,104 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { registerSchema } from "./validation/registerSchema";
+import { useEffect, useRef } from "react";
 import { register } from "../fetch/register";
-export const RegisterForm = ({ styles }) => (
-  <>
-    <Formik
-      initialValues={{
-        name: "",
-        email: "",
-        password: "",
-        nickname: "",
-        phoneNumber: "",
-        profileImg: "",
-      }}
-      validationSchema={registerSchema}
-      onSubmit={(values) => {
-        console.log(values);
-        register(values);
-      }}
-    >
-      <Form>
-        <label className={styles.label} htmlFor="name">
-          Full Name
-        </label>
-        <Field className={styles.field} id="name" name="name" />
-        <ErrorMessage component="a" className={styles.errorMsg} name="name" />
-        <label className={styles.label} htmlFor="Email">
-          Email
-        </label>
-        <Field className={styles.field} id="email" name="email" />
-        <ErrorMessage component="a" className={styles.errorMsg} name="email" />
-        <label className={styles.label} htmlFor="password">
-          password
-        </label>
-        <Field
-          className={styles.field}
-          id="password"
-          name="password"
-          type="password"
-        />
-        <ErrorMessage
-          component="a"
-          className={styles.errorMsg}
-          name="password"
-        />
-        <label className={styles.label} htmlFor="NickName">
-          닉네임
-        </label>
-        <Field className={styles.field} id="nickname" name="nickname" />
-        <ErrorMessage
-          component="a"
-          className={styles.errorMsg}
-          name="nickname"
-        />
-        <label className={styles.label} htmlFor="phoneNumber">
-          휴대폰번호
-        </label>
-        <Field className={styles.field} id="phoneNumber" name="phoneNumber" />
-        <ErrorMessage
-          component="a"
-          className={styles.errorMsg}
-          name="phoneNumber"
-        />
-        <label className={styles.label} htmlFor="profileImg">
-          프로필이미지
-        </label>
+export const RegisterForm = ({ styles }) => {
+  const fileRef = useRef();
+  useEffect(() => {
+    console.log(fileRef.current);
+  }, [fileRef.current]);
+  return (
+    <>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          nickname: "",
+          phoneNumber: "",
+          profileImg: new File([], "profile"),
+        }}
+        validationSchema={registerSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          register(values);
+        }}
+      >
+        <Form>
+          <label className={styles.label} htmlFor="name">
+            Full Name
+          </label>
+          <Field className={styles.field} id="name" name="name" />
+          <ErrorMessage component="a" className={styles.errorMsg} name="name" />
+          <label className={styles.label} htmlFor="Email">
+            Email
+          </label>
+          <Field className={styles.field} id="email" name="email" />
+          <ErrorMessage
+            component="a"
+            className={styles.errorMsg}
+            name="email"
+          />
+          <label className={styles.label} htmlFor="password">
+            password
+          </label>
+          <Field
+            className={styles.field}
+            id="password"
+            name="password"
+            type="password"
+          />
+          <ErrorMessage
+            component="a"
+            className={styles.errorMsg}
+            name="password"
+          />
+          <label className={styles.label} htmlFor="NickName">
+            닉네임
+          </label>
+          <Field className={styles.field} id="nickname" name="nickname" />
+          <ErrorMessage
+            component="a"
+            className={styles.errorMsg}
+            name="nickname"
+          />
+          <label className={styles.label} htmlFor="phoneNumber">
+            휴대폰번호
+          </label>
+          <Field className={styles.field} id="phoneNumber" name="phoneNumber" />
+          <ErrorMessage
+            component="a"
+            className={styles.errorMsg}
+            name="phoneNumber"
+          />
+          <label className={styles.label} htmlFor="profileImg">
+            프로필이미지
+          </label>
 
-        <input
-          type="file"
-          id="profileImg"
-          name="profileImg"
-          accept="img/*"
-          className="hidden"
-        />
+          <input
+            type="file"
+            id="profileImg"
+            name="profileImg"
+            accept="img/*"
+            className="hidden"
+            ref={fileRef}
+          />
 
-        <ErrorMessage
-          component="a"
-          className={styles.errorMsg}
-          name="profileImg"
-        />
+          <ErrorMessage
+            component="a"
+            className={styles.errorMsg}
+            name="profileImg"
+          />
 
-        <button
-          type="submit"
-          className={styles.button}
-          onClick={() => console.log("hi")}
-        >
-          Register
-        </button>
-      </Form>
-    </Formik>
-  </>
-);
+          <button
+            type="submit"
+            className={styles.button}
+            onClick={() => console.log("hi")}
+          >
+            Register
+          </button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
