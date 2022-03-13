de98268c0a8f0ceb7b371c30cc768f7ceb5406dc
eunsun <nammugmuhg@gmail.com>
2022-03-14 05:23:08 +0900

    [#4]feat:로그아웃 서버요청

--- bellytime/fetch/instance.jsx
+++ bellytime/fetch/instance.jsx
@@ -19,24 +19,26 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-  // Add a response interceptor
-  axiosInstance.interceptors.response.use(
-    function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
-      if (response.data.accessToken) {
-        //리코일에 저장하고,
-        //요청다시보내고
-        axiosInstance.defaults.headers = {
-          Authorization: "Bearer " + response.data.accessToken,
-        };
-        return axiosInstance.request(config);
-      }
-      return response;
-    },
-    function (error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
-      return Promise.reject(error);
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    if (response.data.accessToken) {
+      //리코일에 저장하고,
+      //요청다시보내고
+      axiosInstance.defaults.headers = {
+        Authorization: "Bearer " + response.data.accessToken,
+      };
+      return axiosInstance.request(config);
     }
-  );
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+//https://stackoverflow.com/questions/51563821/axios-interceptors-retry-original-request-and-access-original-promise
--- bellytime/fetch/logout.jsx
+++ bellytime/fetch/logout.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import { axiosInstance } from "./instance";
 
 export const logout = async () => {
-  const URL = `/logout`;
+  const URL = `/log-out`;
   try {
-    await axiosInstance.get(URL);
+    await axiosInstance.delete(URL);
     // console.log(response.data.accessToken);
     // window.localStorage.setItem("accessToken", response.data.accessToken);
   } catch (e) {
