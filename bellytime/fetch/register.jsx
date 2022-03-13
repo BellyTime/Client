5ba49c25d5e38f33b90e8f36cc03795b9dc21cb3
eunsun <nammugmuhg@gmail.com>
2022-03-14 05:59:57 +0900

    [#4]feat:회원가입 연결

--- bellytime/fetch/register.jsx
+++ bellytime/fetch/register.jsx
@@ -1,10 +1,27 @@
 import axios from "axios";
 import { loginInstance } from "./instance/loginInstance";
-export const register = async (values) => {
+export const register = async ({
+  name,
+  email,
+  password,
+  nickname,
+  phoneNumber,
+  profileImg,
+}) => {
   const URL = `/join`;
   console.log(values);
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("password", password);
+  formData.append("nickname", nickname);
+  formData.append("phoneNumber", phoneNumber);
+  formData.append("profileImg", profileImg);
+
   try {
-    await loginInstance.post(URL, values);
+    await loginInstance.post(URL, formData, {
+      "content-type": "multipart/form-data",
+    });
   } catch (e) {
     console.log(e);
   }
